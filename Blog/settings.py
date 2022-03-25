8148b3b234581233160667938f8004814a391152
Kevin-Sambuli <sambulikevin@gmail.com>
2022-03-25 13:07:44 +0300

    static

--- Blog/settings.py
+++ Blog/settings.py
@@ -122,15 +122,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'home/media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
